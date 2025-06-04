a5150020bd3dbf8820fb83ff5e215ed7155e9d67
Aishwarya <aishwaryamanojmc@gmail.com>
2025-06-05 00:05:01 +0530

    edited db structure

--- database.py
+++ database.py
@@ -12,3 +12,18 @@ def init_db():
     ''')
     conn.commit()
     conn.close()
+
+def add_note(title,content):
+    conn=sqlite3.connect('notes.db')
+    cursor=conn.cursor()
+    cursor.execute("INSERT INTO notes (title, content) VALUES (?, ?)", (title, content))
+    conn.commit()
+    conn.close()
+
+def get_all_notes():
+    conn=sqlite3.connect('notes.db')
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM notes")
+    rows=cursor.fetchall()
+    conn.close()
+    return rows
